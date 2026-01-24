bda113410f70cce6aa70ea08d58ef29dc5f84e11
Mohitkonchada <mohitkonchada@gmail.com>
2026-01-25 00:56:42 +0530

    Final stable run configuration

--- app.py
+++ app.py
@@ -112,5 +112,7 @@ def logout():
 
 
 # ------------------ RUN ------------------
-with app.app_context():
-    db.create_all()
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(host="0.0.0.0", port=5000)
